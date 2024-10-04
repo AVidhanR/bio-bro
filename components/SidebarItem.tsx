78732610b76bd95905404630b673745ea453aa34
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-04 11:04:50 +0530

    Updated the anchor tag to the next/link tag for better rendering and usage

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,28 +1,31 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 interface SidebarItemProps {
   icon: React.ReactNode;
   isActive: boolean;
   onClick: () => void;
+  linkTo: string;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
   icon,
   isActive,
   onClick,
+  linkTo,
 }) => {
   return (
-    <a
-      href="#cooking-up"
+    <Link
+      href={linkTo}
       onClick={onClick}
       className={`flex items-center p-4 rounded-tl-lg rounded-bl-lg hover:text-blue-500 transition-colors duration-200 ${
         isActive ? "bg-blue-50 text-blue-500" : "text-gray-600"
       }`}
     >
       {icon}
-    </a>
+    </Link>
   );
 };
 
