ee622e04e4cc4b65ed8381f49656f5072aac6045
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-04 20:21:10 +0530

    Update page.tsx

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -1,7 +1,7 @@
 const Data = () => {
   return (
-    <div className="pl-24 mt-8 w-full">
-      <header className="text-2xl">Data</header>
+    <div className="pl-28 mt-12 w-full">
+      <header className="text-3xl text-blue-500">Data</header>
     </div>
   );
 };
