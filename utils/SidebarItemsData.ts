9688f498ff7a4c67cba3f3f2eaa1ce0da7b36878
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-05 14:31:59 +0530

    Added the data for the sidebar components and removed an unused item from the data

--- utils/SidebarItemsData.ts
+++ utils/SidebarItemsData.ts
@@ -2,7 +2,6 @@ interface SidebarItemsDataType {
   id: number;
   linkTo: string;
   icon: React.ReactNode;
-  isActive: boolean;
 }
 
 export const SidebarItemsData: SidebarItemsDataType[] = [
@@ -10,6 +9,20 @@ export const SidebarItemsData: SidebarItemsDataType[] = [
     id: 1,
     linkTo: "/data",
     icon: `<HardDrive size={32} absoluteStrokeWidth={true} />`,
-    isActive: false,
+  },
+  {
+    id: 2,
+    linkTo: "/#registry",
+    icon: `<Library size={32} absoluteStrokeWidth={true} />`,
+  },
+  {
+    id: 3,
+    linkTo: "/#workflows",
+    icon: `<Waypoints size={32} absoluteStrokeWidth={true} />`,
+  },
+  {
+    id: 4,
+    linkTo: "/#pods",
+    icon: `<Icon iconNode={hexagons3} size={32} absoluteStrokeWidth={true} />`,
   },
 ];
